677150c204bdab1a2e5e8d6da87f8fcfe3d12976
shashikant010 <shashikantyadav0503@gmail.com>
2024-03-09 17:22:26 +0530

    added manual validation for email and changed to async await

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const mongoURI='mongodb://localhost:27017/?directConnection=true';
+const mongoURI='mongodb://localhost:27017/inotebook?directConnection=true';
 const connectToMongo = async () => {
     try {
       await mongoose.connect(mongoURI);
--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,16 +1,45 @@
 const express=require('express');
 const router=express.Router();
 const User = require('../models/User');
+const {body,validationResult}=require('express-validator');
 
-router.post('/', (req, res) => {
-  console.log(req.body)
-  const user=User(req.body);
-  user.save();
-  const obj={
-    a:'sky',
-    b:'shashi'
-  }
-    res.json(obj);
+router.post('/createuser',[
+body('name','Enter a valid name').isLength({min:3}),
+body('email','Enter a valid email').isEmail(),
+body('password','Enter a valid password').isLength({min:5})
+] ,async(req, res) => {
+const errors=validationResult(req);
+if(!errors.isEmpty()){
+  return res.status(400).json({error:errors.array()})
+}
+
+try {
+  
+
+
+let user=await User.findOne({email:req.body.email})
+if(user){
+  return res.status(400).json({error:"email of the user is used already"})
+}
+
+
+user=await User.create({
+  name:req.body.name,
+  email:req.body.email,
+  password:req.body.password
+})
+
+res.json(user)
+
+} catch (error) {
+  console.error(error.message)
+  res.status(500).send("some error occured")
+}
+
+//Earlier we were using this .then method but now shifted to async await
+// .then(user=>{res.json(user)}).catch(err=>{console.log(err) 
+//   res.json({error:"please enter unique vlaue",msg:err.message})}  )
+
   })
 
   module.exports = router
